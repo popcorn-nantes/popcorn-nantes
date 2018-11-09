73d62d3a9812b4f8d0e2e08c800485e7144323cb
yann <yann.boisselier@gmail.com>
2018-11-09 22:05:10 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build static version
+npm run generate
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:popcorn-nantes/popcorn-nantes.github.io master
+
+cd -
