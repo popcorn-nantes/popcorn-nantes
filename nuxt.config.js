f66aa23b0192608fa25cc9715f976ff2cf49b959
yann <yann.boisselier@gmail.com>
2018-11-18 21:03:00 +0100

    add twitter cards meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,22 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      // twitter card:
+      { name: 'twitter:card', content: 'summary' },
+      {
+        name: 'twitter:title',
+        content: 'Popcorn : trouvez un·e développeur·e freelance à Nantes'
+      },
+      {
+        name: 'twitter:description',
+        content:
+          'La plateforme avec ( vraiment ) 0% de commission pour tout le monde'
+      },
+      {
+        name: 'twitter:image',
+        content: 'https://popcorn-nantes.github.io/images/popcorn-500'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Score style="padding-top:1em" :persons="persons" />
     <div style="margin-top:2rem" class="container  has-text-centered">
       <h2 class="title">Trouvez un·e développeur·e freelance à Nantes</h2>
       <h3 class="subtitle"><em>Popcorn : La plateforme avec ( vraiment ) 0% de commission pour tout le monde</em></h3>
@@ -14,12 +13,10 @@
 <script>
 import { getPersons } from '../lib/helpers.js'
 import PersonsWithSearch from '../components/PersonsWithSearch'
-import Score from '../components/Score'
 
 export default {
   components: {
-    PersonsWithSearch,
-    Score
+    PersonsWithSearch
   },
   computed: {
     persons: () => getPersons()
