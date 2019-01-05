e5766faf0a935a4100a2c764905e1f0cf21816be
yann <yann.boisselier@gmail.com>
2019-01-05 15:26:33 +0100

    update to gustave 0.0.16

--- importers/pages.js
+++ importers/pages.js
@@ -1,5 +1,5 @@
 const { parseMarkdownDirectory } = require('nuxt-gustave/lib/markdown')
-const { saveToJsonDirectory } = require('nuxt-gustave/lib/helpers')
+const { saveToJsonDirectory } = require('nuxt-gustave/lib/gustave')
 
 exports.importer = () => {
   const resources = parseMarkdownDirectory('content/pages')
