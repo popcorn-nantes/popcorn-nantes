03535ff7ff92b24a81c280302aeff2d856df79b9
yann <yann.boisselier@gmail.com>
2019-01-09 13:04:28 +0100

    factorisation headers pour les réseaux sociaux

--- services/helpers.js
+++ services/helpers.js
@@ -9,3 +9,52 @@ export function shuffle(a) {
   }
   return a
 }
+
+export function getSocialShareHeadersMeta({ title, description, image, url }) {
+  return [
+    // open graph
+    { hid: 'og:site_name', name: 'og:site_name', content: 'Popcorn' },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: url
+    },
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: title
+    },
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: process.env.POPCORN_BASE_URL
+    },
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: image
+    },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: description
+    },
+    // twitter card:
+    { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: title
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: description
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: image
+    }
+  ]
+}
