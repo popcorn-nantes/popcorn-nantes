8812ebe92b9903c3d5d472684e88633cd6429508
yann <yann.boisselier@gmail.com>
2019-01-14 10:07:30 +0100

    fix tests

--- test/e2e/integration/tests.js
+++ test/e2e/integration/tests.js
@@ -9,10 +9,8 @@ describe('Tests', () => {
       cy.get('h3').contains("10 ans d'expérience")
       cy.get('.domaines-metiers').contains('sites web')
       cy.get('.technologies').contains('Vue')
-      cy.get('.technologies').contains('React')
       cy.get('.technologies').contains('GraphQL')
       cy.get('.technologies').contains('Nuxt')
-      cy.get('.technologies').contains('Next')
       cy.get('.button').click()
       cy.url().should('contain', '/person/yann-boisselier') // => true
     })
@@ -22,10 +20,8 @@ describe('Tests', () => {
     cy.get('h1').contains('Yann Boisselier')
     cy.get('h2').contains('Développeur JavaScript FullStack')
     cy.get('.technologies').contains('Vue')
-    cy.get('.technologies').contains('React')
     cy.get('.technologies').contains('GraphQL')
     cy.get('.technologies').contains('Nuxt')
-    cy.get('.technologies').contains('Next')
     cy.get('.domaines-metiers').contains('sites web')
   })
   it('Click on contact person in profile page', () => {
