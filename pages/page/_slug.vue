5f924a67f0745b45a7df09adb42e9b98d3cc8a0c
yann <yann.boisselier@gmail.com>
2019-01-14 14:34:22 +0100

    remove page component

--- pages/page/_slug.vue
+++ pages/page/_slug.vue
@@ -5,13 +5,9 @@
 </template>
 
 <script>
-import Page from '@/components/Page'
 import { getPageBySlug } from '@/services/content'
 
 export default {
-  components: {
-    Page
-  },
   computed: {
     page() {
       return getPageBySlug(this.$route.params.slug)
