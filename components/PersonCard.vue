fd377ee3a1aa2bc4b5f84b8beaf53c56fe59ea2a
Aurélien <aurelien@laumiere.net>
2019-01-16 14:23:42 +0100

    fix(a11y): imbrication de liens bloquante pour navigation au clavier

--- components/PersonCard.vue
+++ components/PersonCard.vue
@@ -1,37 +1,33 @@
 <template>
   <div class="person" :id="`person-${person.$slug}`">
-    <nuxt-link
-      :to="{path: `/person/${person.$slug}`, query: { search: $store.getters.currentSearch }}"
-    >
-      <div class="card">
-        <PersonCardPhoto :person="person"/>
+    <div class="card" @click="detail()">
+      <PersonCardPhoto :person="person"/>
 
-        <div class="card-content">
-          <h2 style="font-size:20px;" class="has-text-centered">{{person.titre}}</h2>
-          <h3
-            v-show="person.sous_titre"
-            style="font-size:15px;font-style:italic"
-            class="has-text-centered"
-          >{{person.sous_titre}}</h3>
-          <div class="has-text-centered" style="padding-top: 1em">
-            <Tags class="domaines-metiers" :tags="person.domaines_metiers"/>
-          </div>
+      <div class="card-content">
+        <h2 style="font-size:20px;" class="has-text-centered">{{person.titre}}</h2>
+        <h3
+          v-show="person.sous_titre"
+          style="font-size:15px;font-style:italic"
+          class="has-text-centered"
+        >{{person.sous_titre}}</h3>
+        <div class="has-text-centered" style="padding-top: 1em">
+          <Tags class="domaines-metiers" :tags="person.domaines_metiers"/>
+        </div>
 
-          <div class="has-text-centered" style="padding-top: 1em">
-            <Tags class="technologies" :tags="person.technologies"/>
-          </div>
-          <div class="content">
-            <div class="has-text-centered">
-              <br>
-              <nuxt-link
-                :to="{path: `/person/${person.$slug}`,query: { search: $store.getters.currentSearch}}"
-                class="button is-primary"
-              >Voir le profil</nuxt-link>
-            </div>
+        <div class="has-text-centered" style="padding-top: 1em">
+          <Tags class="technologies" :tags="person.technologies"/>
+        </div>
+        <div class="content">
+          <div class="has-text-centered">
+            <br>
+            <nuxt-link
+              :to="{path: `/person/${person.$slug}`,query: { search: $store.getters.currentSearch}}"
+              class="button is-primary"
+            >Voir le profil</nuxt-link>
           </div>
         </div>
       </div>
-    </nuxt-link>
+    </div>
   </div>
 </template>
 
@@ -49,6 +45,11 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    detail() {
+      this.$router.push({path: `/person/${this.person.$slug}`,query: { search: this.$store.getters.currentSearch}});
+    }
   }
 }
 </script>
@@ -58,6 +59,7 @@ export default {
   transition: all 0.2s ease-in-out;
   top: -5px;
   box-shadow: 0 2px 20px;
+  cursor: pointer;
 }
 .card {
   height: 100%;
