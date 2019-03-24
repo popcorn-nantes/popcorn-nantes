3b1134f75caaaef7f11f89c8ebcd7082aa153120
yann <yann.boisselier@gmail.com>
2019-03-24 21:47:10 +0100

    set matomo config

--- popcorn.config.js
+++ popcorn.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   location: "à Nantes",
   matomo: {
-    matomoUrl: '//analytics.komuneco.fr/',
-    siteId: 5,
+    matomoUrl: 'https://popcorn-nantes.matomo.cloud/',
+    siteId: 1,
     debug: true,
     verbose: true,
   }
